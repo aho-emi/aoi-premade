3c850e3ceae352661c48c39691280d68ef073617
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-09-05 13:01:30 +0000

    seesh

--- node_modules/@akarui/aoi.music/lib/newutils/search.d.ts
+++
@@ -1,3 +0,0 @@
-import { Manager } from "../newstruct/manager";
-import { PlatformType } from "../typings/enums";
-export declare function search(query: string, type: PlatformType, manager: Manager): Promise<string[]>;
